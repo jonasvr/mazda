ae366119d84f8cce35856685515cfb0980c2949f
Van Reeth <jonas.vanreeth@yrbrands.com>
2016-04-20 17:26:37 +0200

    working back up

--- www/js/location.js
+++ www/js/location.js
@@ -116,7 +116,37 @@ function checkStartLocation(){
     }
 }
 
+// function speedCheck(){
+//     console.log('in');
+//     // onSuccess Callback
+//     //   This method accepts a `Position` object, which contains
+//     //   the current GPS coordinates
+//     //
+//     function onSuccess(position) {
+//         var element = document.getElementById('geolocation');
+//         element.innerHTML = 'Latitude: '  + position.coords.latitude      + '<br />' +
+//                             'Longitude: ' + position.coords.longitude     + '<br />' +
+//                             '<hr />'      + element.innerHTML;
+//     }
+//
+//     // onError Callback receives a PositionError object
+//     //
+//     function onError(error) {
+//         alert('code: '    + error.code    + '\n' +
+//               'message: ' + error.message + '\n');
+//     }
+//
+//     // Options: throw an error if no update is received every 30 seconds.
+//     //
+//     // var watchID = GPSLocation.watchPosition(onSuccess, onError, { timeout: 3000, enableHighAccuracy: true  });
+//     var geo = cordova.require('cordova/plugin/geolocation');
+// geo.getCurrentPosition(win, fail,opts);
+// }
+
+
+
 function speedCheck(){
+    console.log('in');
     // onSuccess Callback
     //   This method accepts a `Position` object, which contains
     //   the current GPS coordinates
